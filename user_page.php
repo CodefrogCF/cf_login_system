26741f9b9d0d43e1ed0fc9d68f426fc81458467d
HagenK42 <666.thurisaz.666@gmail.com>
2025-03-30 21:54:04 +0200

    added logout functionality and styled the user/admin landing-page

--- user_page.php
+++ user_page.php
@@ -1,5 +1,16 @@
+<?php
+
+session_start();
+if (!isset($_SESSION['email'])) {
+    header("Location: index.php");
+    exit();
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -8,7 +19,22 @@
     <link rel="icon" href="./assets/favicon.ico">
     <link rel="stylesheet" href="styles.css">
 </head>
+
 <body>
-    <h1>User Page</h1>
+
+    <div class="header">
+        <a href="#"><img src="./assets/images/logo-192x192.webp" class="logo"></a>
+        <a href="#"><h1>LoginForm</h1></a>
+    </div>
+
+    <div class="container">
+        <div class="box">
+            <h1 class="welcome-text">Welcome, <span><?= $_SESSION['name'] ?></span></h1>
+            <p>This is an <span>user</span> page</p>
+            <button onclick="window.location.href='logout.php'">Logout</button>
+        </div>
+    </div>
+
 </body>
+
 </html>
