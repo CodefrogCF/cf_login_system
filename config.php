17c20b5f936a0590c67cfc2bc869df732dcc7585
HagenK42 <666.thurisaz.666@gmail.com>
2025-04-04 10:27:31 +0200

    bugfixing

--- config.php
+++ config.php
@@ -1,14 +1,9 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__);
-$dotenv->load();
-
-$host = $_ENV['SQL_HOST'];
-$user = $_ENV['SQL_USER'];
-$password = $_ENV['SQL_PASSWORD'];
-$database = $_ENV['SQL_DATABASE'];
+$host = "webspace34.do.de";
+$user = "web33";
+$password = "g3Mp,=KH69+a";
+$database = "usr_web33_2";
 
 $conn = new mysqli($host, $user, $password, $database);
 
